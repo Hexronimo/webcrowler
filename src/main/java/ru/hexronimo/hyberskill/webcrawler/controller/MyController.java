7ad93e6c21149447bfde8f6087e0cb0323ee6899
Hexronimo <leafcat@mail.ru>
2019-10-04 11:49:15 +0300

    Hiberskill's 3 step

--- src/main/java/ru/hexronimo/hyberskill/webcrawler/controller/MyController.java
+++ src/main/java/ru/hexronimo/hyberskill/webcrawler/controller/MyController.java
@@ -9,37 +9,50 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
 
-	@RequestMapping(value = "/")
+	@GetMapping(value = "/")
 	public String mainForm() {
 		return "index";
 	}
 
-	@RequestMapping(value = "/submit")
-	public String mainForm(@RequestParam(value = "url") String url) {
-		InputStream inputStream;
-		try {
-			inputStream = new URL(url).openStream();
+	@PostMapping(value = "/")
+	public String mainForm(@RequestParam(value = "url") String url, Model model) {
+		String siteText = ""; 
+		try(InputStream inputStream = new URL(url).openStream();) {
+			String LINE_SEPARATOR = "\n";
 			BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 			StringBuilder stringBuilder = new StringBuilder();
 			String nextLine;
 			while ((nextLine = reader.readLine()) != null) {
 				stringBuilder.append(nextLine);
-				stringBuilder.append("\n");
+				stringBuilder.append(LINE_SEPARATOR);
 			}
 
-			String siteText = stringBuilder.toString();
-			System.out.println(siteText);
+			siteText = stringBuilder.toString();
 		} catch (MalformedURLException e) {
+			System.out.println();
+			System.out.println("Something wrong with this URL");
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		model.addAttribute("htmlbody", siteText);
+		// From <title> to </title>
+		int titlestart = siteText.indexOf("<title>") + 7;
+		int titleend = siteText.indexOf("</title>");
+		String title = "";
+		if (titlestart != -1 && titleend != -1) {
+			title = "Title:" + siteText.substring(titlestart,titleend);
+		}
+		model.addAttribute("sitetitle", title);
 		return "index";
 	}
 }
